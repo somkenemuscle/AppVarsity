cc63f1ad62a362969c0eef931d88b11064b71616
Somkene Ojukwu <somkeneoj@gmail.com>
2024-08-06 04:51:51 +0100

    2:working on homepage:footer/accordion

--- components/shared/Features.tsx
+++ components/shared/Features.tsx
@@ -10,7 +10,7 @@ export default function Features() {
                     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                         <div className="lg:pr-8 lg:pt-4">
                             <div className="lg:max-w-lg">
-                                <h2 className="text-base font-semibold leading-7 text-gray-500">Some of our resources <span aria-hidden="true">→</span></h2>
+                                <h2 className="text-base font-thin font-mono leading-7 text-gray-500">Some of our resources <span aria-hidden="true">→</span></h2>
                                 <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Get access to past project documentation.</p>
                                 <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                                     {features.map((feature) => (
