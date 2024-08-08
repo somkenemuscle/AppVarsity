a530c52cd8d33066e08f79aa56f52b7711509266
Somkene Ojukwu <somkeneoj@gmail.com>
2024-08-08 02:03:37 +0100

    2:working on homepage:interactions

--- constants/interactionFeature.ts
+++ constants/interactionFeature.ts
@@ -1,14 +1,15 @@
 
 export const interactionFeature = [
+   
+    {
+        name: 'Free Programming Courses ->',
+        description: 'We will give you access to a curated selection of paid programming course videos for free.  Simply reach out to us through your preferred method, and we will gladly provide you with the resources',
+        icon: '->',
+    },
     {
 
         name: 'Community Q&A ->',
         description: 'You can also share your questions with our community and receive responses from fellow students and our team. Get help, exchange ideas, and engage with others for a collaborative learning experience.',
-        icon: '->',
-    },
-    {
-        name: 'Free Programming Courses ->',
-        description: 'We will give you access to a curated selection of paid programming course videos for free.  Simply reach out to us through your preferred method, and we will gladly provide you with the resources',
         icon: '->',
     }
 ];
