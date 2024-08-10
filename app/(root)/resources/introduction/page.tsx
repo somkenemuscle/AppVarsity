5bf0508dd128651bada425134f4ac3163f111a7f
Somkene Ojukwu <somkeneoj@gmail.com>
2024-08-11 00:13:17 +0100

    5:working on project page

--- app/(root)/resources/introduction/page.tsx
+++ app/(root)/resources/introduction/page.tsx
@@ -11,8 +11,8 @@ function IntroductionPage() {
         This section is designed to guide you through the essentials you need to get started and make the most of your studies.</p>
 
       <p className="font-normal">
-        <span className="font-medium">Kindly Note <span aria-hidden="true">→</span> </span> Only the Project guide section is currently available as it was made a priority due to the emergence of the project for ADSE batch 846. New resources will
-        be added gradually as time goes, but we have made the project guide available for use to assist batch 846 with their semester one project.
+        <span className="font-medium">Kindly Note <span aria-hidden="true">→</span> </span> Only the Project guide section is currently available as it was made a priority due to the emergence of the project for ADSE batch 853. New resources will
+        be added gradually as time goes, but we have made the project guide available for use to assist batch 853 with their semester one project.
       </p>
     </div>
   )
--- app/(root)/resources/projectguide/overview/page.tsx
+++ app/(root)/resources/projectguide/overview/page.tsx
@@ -1,4 +1,8 @@
 import Breadcrumb from "@/components/shared/Breadcrumb"
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+import { ChevronRight } from "lucide-react"
 
 function OverviewPage() {
   return (
@@ -6,8 +10,43 @@ function OverviewPage() {
       <Breadcrumb
         page='Overview'
       />
-      OverviewPage
+      <h1 className="font-semibold text-3xl mb-2">Project Overview</h1>
+      <p className="mb-5 text-gray-400 font-light">Each semester, students are grouped and assigned projects that utilize various technologies
+        taught in school. We provide comprehensive guides and overviews to help you approach your projects effectively. These guides are derived
+        from a combination of past high-scoring projects, ensuring you receive valuable insights and practical tips.
+      </p>
+
+      <h1 className="font-semibold text-3xl mb-2">Get Started On Your Project</h1>
+      <p className="font-normal">
+        The group representative for that batch will receive an email containing instructions on how their group should approach their project.
+        The email should look something
+        like this
+      </p>
+
+      <div className="relative w-full mt-7">
+        <Image
+          alt="email-pic"
+          layout="responsive"
+          width={1200}
+          height={800}
+          src='/assets/images/email1.jpg'
+          className="rounded-md"
+        />
       </div>
+
+      <p className="font-normal mt-7">
+        NB: Each student should respond to the email <span className="text-indigo-400">eprojects@aptech.ac.in</span>  , acknowledging that they have received their e-project.
+        Just send back something in this format <span className="text-indigo-400">'Good day, I am Ojukwu Somkene and i am acknowledging the e-project'</span>
+      </p>
+
+      <Link href="/resources/projectguide/details" passHref>
+        <Button className="mt-5 font-light bg-blue-200 border border-gray-800 rounded-md hover:bg-gray-700 flex items-center">
+          Details <ChevronRight className="inline-block ml-1" />
+        </Button>
+      </Link>
+
+
+    </div>
   )
 }
 
