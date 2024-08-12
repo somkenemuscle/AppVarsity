9e27aab72c34b3f13244126cd987b8030c4545ff
Somkene Ojukwu <somkeneoj@gmail.com>
2024-08-12 20:35:58 +0100

    10: xml and json  past questions implemented

--- app/(root)/resources/examination/pastquestions/page.tsx
+++ app/(root)/resources/examination/pastquestions/page.tsx
@@ -1,7 +1,5 @@
 import Breadcrumb from "@/components/shared/Breadcrumb"
 import Link from "next/link"
-import { ChevronRight, ChevronLeft } from "lucide-react"
-import { Button } from "@/components/ui/button"
 
 
 function PastQuestionsPage() {
@@ -12,7 +10,33 @@ function PastQuestionsPage() {
                 page='Past Questions'
             />
 
-            <p className="text-2xl text-gray-400 font-light">NB: XML Past questions will be uploaded this evening</p>
+            <h1 className="font-semibold text-3xl mb-2">Past Questions</h1>
+            <p className="mb-5 text-gray-400 font-light"> Kindly note: Only XML and JSON past questions are
+                available due to the ADSE Batch 853 exam, scheduled for August 14, 2024 (Wednesday).
+            </p>
+
+            <p className="font-light">
+                You can find a link to the past question in a PDF format below :
+            </p>
+
+
+            <Link
+                href='https://utfs.io/f/7bd792a4-c003-40bb-b958-1bd7d27e1d98-3lypy.pdf'
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-indigo-500 hover:underline"
+            >
+                <p>
+                    Xml And Json Past Question Pdf
+                </p>
+            </Link>
+
+            <p className="font-light mt-5">
+                In the coming days, we will update this page with all the remaining past 
+                questions. Good luck on your exams, courtesy of Appvarsity.
+            </p>
+
+
         </div>
     )
 }
