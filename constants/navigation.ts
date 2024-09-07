10031412129696f2cc969af2da3a515ac651ab9f
Somkene Ojukwu <somkeneoj@gmail.com>
2024-09-07 02:47:14 +0100

    Interactions page removed

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -2,7 +2,6 @@
 export const navigation = [
     { name: 'Resources', href: '/resources/introduction' },
     { name: 'Contact Us', href: '#contact-us' },
-    { name: 'Interactions', href: '/interactions' },
     { name: 'FAQs', href: '#faqs' }
 ];
 
