c9f1a552fcfa4d3ee1c5aad0bb83ce0084a3afab
Somkene Ojukwu <somkeneoj@gmail.com>
2024-10-03 17:47:52 +0100

    past questions in correct order

--- constants/pastQuestionPdfLinks.ts
+++ constants/pastQuestionPdfLinks.ts
@@ -1,9 +1,5 @@
 export const pastQuestionPdflinks = [
     {
-        href: 'https://utfs.io/f/7bd792a4-c003-40bb-b958-1bd7d27e1d98-3lypy.pdf',
-        course: ' Xml And Json Past Question Pdf [Old Curriculumn]'
-    },
-    {
         href: 'https://utfs.io/f/7f84cefc-971e-40a9-acc8-6a4ab96485e1-rwchdi.pdf',
         course: ' Ppt Past Question Pdf'
     },
@@ -18,6 +14,10 @@ export const pastQuestionPdflinks = [
     {
         href: 'https://utfs.io/f/db53fa55-83e6-48b0-9f50-bb332c3f7751-rwchg3.pdf',
         course: ' UI/UX Past Question Pdf'
+    },
+    {
+        href: 'https://utfs.io/f/7bd792a4-c003-40bb-b958-1bd7d27e1d98-3lypy.pdf',
+        course: ' Xml And Json Past Question Pdf [Old Curriculumn]'
     },
     {
         href: 'https://utfs.io/f/rXxdhjbFRcsm0a498DiJjFlTbaKcgZwWY1xqVXMnQDNdr6tv',
