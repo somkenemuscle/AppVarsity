eb6472dbdc646403ede814c43e9d9bd86e92aac5
Somkene Ojukwu <somkeneoj@gmail.com>
2025-01-20 14:11:11 +0100

    Homepage ui updated

--- components/shared/Services.tsx
+++ components/shared/Services.tsx
@@ -6,7 +6,6 @@ export default function Services() {
             <div id='service' className="bg-white py-24 sm:py-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:text-center">
-                        <h2 className="text-sm font-mono leading-7 text-gray-500">Your Learning, Our Support</h2>
                         <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                             We Provide the Info, You Achieve the <span className='text-orange-400'>Success</span>
                         </p>
