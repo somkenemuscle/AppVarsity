bec8c6069574c9610779ef994b7e88551c9d724b
Somkene Ojukwu <somkeneoj@gmail.com>
2025-02-09 23:47:07 +0100

    Testing PWA Config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,35 @@
+
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import WhatsappIcon from "@/components/shared/WhatasappIcon";
-
-const raleway = Raleway({ subsets: ["latin"] });
+// import { useEffect } from "react";
 
 
 
 export const metadata: Metadata = {
   title: "Appvarsity",
   description: "Your guide to an easy aptech life",
+  manifest: '/web.manifest.json'
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+  
+  // useEffect(() => {
+  //   if ("serviceWorker" in navigator) {
+  //     navigator.serviceWorker
+  //       .register("/service-worker.ts")
+  //       .then((registration) => {
+  //         console.log("Service Worker registered with scope:", registration.scope);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Service Worker registration failed:", error);
+  //       });
+  //   }
+  // }, []);
+
+
   return (
     <>
       <ClerkProvider>
