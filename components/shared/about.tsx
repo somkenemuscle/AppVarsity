eee9fe532c70d551ae1bcc6c2636986596be2f20
somkenemuscle <somkeneoj@gmail.com>
2026-09-14 18:44:35 +0100

    Add leading icon to Access Questions button

--- components/shared/about.tsx
+++ components/shared/about.tsx
@@ -38,6 +38,7 @@ questions.
                     <div className='mt-8'>
                         <Link href='/resources/examination/pastquestions'>
                             <button className='group inline-flex items-center gap-2 bg-[#4f46e5] hover:bg-[#6366f1] transition-[background,transform,box-shadow] hover:-translate-y-0.5 text-white px-5 py-3 text-sm font-medium shadow-[0_2px_0_#3730a31c]'>
+                                <FileQuestion className="w-4 h-4" />
                                 Access Questions
                                 <ArrowUpRight className="w-4 h-4 transition-transform duration-200 group-hover:translate-x-0.5 group-hover:-translate-y-0.5" />
                             </button>
