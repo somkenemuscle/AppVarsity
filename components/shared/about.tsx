f93ca4296d9fbb80074351b37bc0ac62dd6321fd
somkenemuscle <somkeneoj@gmail.com>
2026-09-14 19:08:18 +0100

    Remove leading icon from Access Questions button and adjust padding

--- components/shared/about.tsx
+++ components/shared/about.tsx
@@ -36,8 +36,7 @@ function About() {
 
                     <div className='mt-8'>
                         <Link href='/resources/examination/pastquestions'>
-                            <button className='group inline-flex items-center gap-2 bg-[#4f46e5] hover:bg-[#6366f1] transition-[background,transform,box-shadow] hover:-translate-y-0.5 text-white px-5 py-3 text-sm font-medium shadow-[0_2px_0_#3730a31c]'>
-                                <FileQuestion className="w-4 h-4" />
+                            <button className='group inline-flex items-center gap-2 bg-[#4f46e5] hover:bg-[#6366f1] transition-[background,transform,box-shadow] hover:-translate-y-0.5 text-white px-6 py-3.5 text-sm font-medium shadow-[0_2px_0_#3730a31c]'>
                                 Access Questions
                                 <ArrowUpRight className="w-4 h-4 transition-transform duration-200 group-hover:translate-x-0.5 group-hover:-translate-y-0.5" />
                             </button>
